03a8ee87e3c7beb41c4d2b5e19f65663a089ea13
peterhelin <helin199210@outlook.com>
2016-05-10 11:27:04 +0800

    setup env

--- install-mac-php-mysql-nginx.sh
+++ install-mac-php-mysql-nginx.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo "hello"
+
+#1.安装homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#2.安装nginx
+brew install nginx
+
+#3.配置nginx
+cp -r conf.d /usr/local/etc/nginx
+cp nginx.conf /usr/local/etc/nginx
+
+#4.配置hosts文件
+echo "127.0.0.1	localhost.lc" >> /etc/hosts
+echo "127.0.0.1	testing.lc" >> /etc/hosts
+
+#5.安装php
+cp php-fpm.conf /private/etc/
+
+#6.安装mysql
+brew install mysql
+
+#7.复制代码
+mkdir /wwwroot
+cp -r localhost /wwwroot
+curl -i "localhost.lc"
