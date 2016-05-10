39e712c2f7d244e54d746668b05fde85c76e5649
张栋 <zhangdong@zhangdongdeMacBook-Pro.local>
2016-05-10 12:29:02 +0800

    update shell

--- install-mac-php-mysql-nginx.sh
+++ install-mac-php-mysql-nginx.sh
@@ -7,20 +7,28 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install nginx
 
 #3.配置nginx
-cp -r conf.d /usr/local/etc/nginx
-cp nginx.conf /usr/local/etc/nginx
+sudo cp -r conf.d /usr/local/etc/nginx
+sudo cp nginx.conf /usr/local/etc/nginx
+sudo mkdir -p /var/log/nginx
+sudo touch /var/log/nginx/localhost.log
 
 #4.配置hosts文件
-echo "127.0.0.1	localhost.lc" >> /etc/hosts
-echo "127.0.0.1	testing.lc" >> /etc/hosts
+sudo echo "127.0.0.1	localhost.lc" >> /etc/hosts
+sudo echo "127.0.0.1	testing.lc" >> /etc/hosts
 
 #5.安装php
-cp php-fpm.conf /private/etc/
+sudo cp php-fpm.conf /private/etc/
 
 #6.安装mysql
 brew install mysql
 
 #7.复制代码
-mkdir /wwwroot
-cp -r localhost /wwwroot
+sudo mkdir /wwwroot
+sudo cp -r localhost /wwwroot
+sudo chmod -R 777 wwwroot
+
+#8.开启服务
+sudo nginx
+sudo php-fpm
+
 curl -i "localhost.lc"
